3cbe23c4d80f9296e47a45686d5b5d7e7d58e033
Yating <nicolezhangyt91@gmail.com>
2020-03-01 11:20:40 +1100

    init commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('a test recipe', 'this is some dexc', 'https://cdn.loveandlemons.com/wp-content/uploads/2017/10/IMG_0071.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
