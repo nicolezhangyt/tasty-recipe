1895bc0747fa00c83c343690f03913dd98e3a317
Yating <nicolezhangyt91@gmail.com>
2020-03-01 22:44:44 +1100

    feat: shopping list item added function

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('a test recipe', 'this is some dexc', 'https://cdn.loveandlemons.com/wp-content/uploads/2017/10/IMG_0071.jpg')
+    new Recipe('a test recipe', 'this is some dexc', 'https://cdn.loveandlemons.com/wp-content/uploads/2017/10/IMG_0071.jpg'),
+    new Recipe('another test recipe', 'this is some dexc', 'https://cdn.loveandlemons.com/wp-content/uploads/2017/10/IMG_0071.jpg'),
+
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
