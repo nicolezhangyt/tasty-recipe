95468d8e7179d46a83f0649922560cfb7ab27d1e
Yating <nicolezhangyt91@gmail.com>
2020-04-11 23:55:00 +1000

    fix: environment variable use incorrectly

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
 
   signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=environment.firebaseAPIKey',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
         {
           email,
           password,
@@ -47,7 +47,7 @@ export class AuthService {
 
   login(email: string, password: string) {
      return this.http.post<AuthResponseData>(
-       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=environment.firebaseAPIKey',
+       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
       {
         email,
         password,
